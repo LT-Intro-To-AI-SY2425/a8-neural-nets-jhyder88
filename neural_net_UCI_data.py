9b98a3c0a7da6a591a83002d0b8b53855b2548a8
Joun Hyder <joun88hyder@gmail.com>
2025-04-25 11:03:29 -0500

    yuyy7y7

--- neural_net_UCI_data.py
+++ neural_net_UCI_data.py
@@ -48,10 +48,11 @@ def normalize(data: List[Tuple[List[float], List[float]]]):
 with open("wine_data.txt", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-td = normalize(training_data)
-
+#td = normalize(training_data)
+train_data_norm = normalize(train_data_split)
+test_data_norm = normalize(test_data_split)
 nn = NeuralNet(13, 3, 1)
-nn.train(td, iters=10000, print_interval=1000, learning_rate=0.1)
+nn.train(train_data_norm, iters=10000, print_interval=1000, learning_rate=0.5)
 
-for i in nn.test_with_expected(td):
+for i in nn.test_with_expected(test_data_norm):
     print(f"desired: {i[1]}, actual: {i[2]}")
